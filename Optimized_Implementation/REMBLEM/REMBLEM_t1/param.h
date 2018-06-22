c507c8cd22d02a887c9341a0ca2abd090c3d7b19
PQCEmblem <40163003+PQCEmblem@users.noreply.github.com>
2018-06-22 11:37:36 +0900

    Add files via upload

---
+++ Optimized_Implementation/REMBLEM/REMBLEM_t1/param.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define R_14_1
+
+#ifdef R_14_1
+
+#define CRYPTO_R_n 1024
+#define CRYPTO_R_k 1
+#define CRYPTO_R_logq 14
+#define CRYPTO_R_sigma 3
+#define CRYPTO_R_t 1
+#define CRYPTO_R_NTT ntt_1024_12289
+#define CRYPTO_R_INTT inv_ntt_1024_12289
+#define CRYPTO_R_q 12289
+#define CRYPTO_R_msg 256
+#define CRYPTO_RGen 3
+#define CRYPTO_Rm 2
+#define CRYPTO_N_INV 12277
+
+#endif
+
+#ifdef R_14_2
+
+#define CRYPTO_R_n 1024
+#define CRYPTO_R_k 1
+#define CRYPTO_R_logq 14
+#define CRYPTO_R_sigma 3
+#define CRYPTO_R_t 1
+#define CRYPTO_R_NTT ntt_1024_12289
+#define CRYPTO_R_INTT inv_ntt_1024_12289
+#define CRYPTO_R_q 12289
+#define CRYPTO_R_msg 256
+#define CRYPTO_RGen 5
+#define CRYPTO_Rm 3
+#define CRYPTO_N_INV 12277
+
+#endif
+
+typedef struct
+{
+	int *A;
+	int *B;
+
+}_CRYPTO_R_pub_struct;
+
+typedef _CRYPTO_R_pub_struct CRYPTO_R_pub_t[1];
